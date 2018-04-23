aff7beb428d75d7614f3aeb36b87900329b33616
saurabhmodh94 <saurabhmodh94@gmail.com>
2018-04-23 23:26:29 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
-      </div>
+      <MuiThemeProvider>
+        <Navbar />
+      </MuiThemeProvider>
     );
   }
 }
